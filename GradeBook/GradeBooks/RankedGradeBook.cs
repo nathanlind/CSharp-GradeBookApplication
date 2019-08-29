72f7675b312476923e8570b5c252dbdfe0eb2f8d
Nathan Lindsey <Nathan.Lindsey@jbhunt.com>
2019-08-29 10:30:08 -0500

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,7 +13,7 @@ namespace GradeBook.GradeBooks
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
-                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+                throw new InvalidOperationException("Rankedgrading requires at least 5 students");
 
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
@@ -28,6 +28,28 @@ namespace GradeBook.GradeBooks
                 return 'D';
             else
                 return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
         }
     }
 }
